8d2b729093152378324b438290d7a68e340ffca5
Phil Hawker <philhawker1211@gmail.com>
2020-02-25 13:25:47 -0700

    added all required nav pages including slug and no-match

--- src/components/app.js
+++ src/components/app.js
@@ -1,4 +1,7 @@
 import React, { Component } from 'react';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
+
+
 
 export default class App extends Component {
   render() {
